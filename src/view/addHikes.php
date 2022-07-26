525c059f482d47fba5e1aa7c8183bb69259581ee
Lydia <lydia.haway@gmail.com>
2022-07-26 12:12:01 +0200

    ex

--- src/view/addHikes.php
+++ src/view/addHikes.php
@@ -10,6 +10,46 @@
 
 <body>
 
+    <main>
+        <form method="POST" action="">
+            <div>
+                <label for="name">Titre </label>
+                <input type="text" name="name">
+            </div>
+
+            <div>
+                <label for="distance">Distance </label>
+                <input type="number" name="distance">
+            </div>
+
+            <div>
+                <label for="elevation_gain">Elevation positive </label>
+                <input type="number" name="elevation_gain">
+            </div>
+
+            <div>
+                <label for="description">Description </label>
+                <textarea name="description" id="description" cols="15" rows="5"></textarea>
+            </div>
+
+            <div>
+                <label for="location">Commune </label>
+                <input type="text" name="location">
+            </div>
+
+            <div>
+                <label for="tags">Tags </label>
+                <select>
+                    <option></option>
+                </select>
+            </div>
+
+            <br>
+
+            <button type="submit">Ajouter</button>
+        </form>
+    </main>
+
 </body>
 
 </html>
