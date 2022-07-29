e7dd202029a3c2a0a5ad9358d5237e1aab7a0b76
Lydia <lydia.haway@gmail.com>
2022-07-29 11:44:27 +0200

    ex

--- src/core/routes.php
+++ src/core/routes.php
@@ -8,7 +8,7 @@ $routes = [
         '/hike' => '../controller/hike_controller.php',
         '/formulaire_connection' => '../controller/controller_form_connection.php',
         '/tag' => '../controller/filter_tags_controller.php',
-        '/form_inscription' => '../controller/form_inscription_controller.php',
+        '/form_inscription' => '../controller/controller_form_connection.php',
     ],
 
     'POST' => [
