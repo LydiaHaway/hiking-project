f8e5c6db7e98fe85500232c9e4045e699bddacb0
Lydia <lydia.haway@gmail.com>
2022-08-03 10:48:51 +0200

    ex

--- src/view/user_control.php
+++ src/view/user_control.php
@@ -9,56 +9,55 @@ require_once '../view/include/header.php';
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
-    <h1>
-        Bonjour <?php echo $_SESSION['LOGGED_USER']['firstname']; ?> et bienvenue sur votre page admin !
-    </h1>
+<h1>
+    Bonjour <?php echo $_SESSION['LOGGED_USER']['firstname']; ?> et bienvenue sur votre page admin !
+</h1>
 
-    <p>
-        Voici les utilisateurs de votre site :
-    </p>
+<p>
+    Voici les utilisateurs de votre site :
+</p>
 
-    <?php
-    foreach ($users->getUsers() as $key => $user) {
-    ?>
+<?php
+foreach ($users->getUsers() as $key => $user) {
+?>
 
-        <div class="users">
-            <h2>
-                <?php echo htmlspecialchars($user['firstname']); ?>
-            </h2>
+    <div class="users">
+        <h2>
+            <?php echo htmlspecialchars($user['firstname']); ?>
+        </h2>
 
-            <h3>
-                <?php echo htmlspecialchars($user['lastname']); ?>
-            </h3>
+        <h3>
+            <?php echo htmlspecialchars($user['lastname']); ?>
+        </h3>
 
-            <p>
-                <?php echo htmlspecialchars($user['nickname']); ?>
-            </p>
+        <p>
+            <?php echo htmlspecialchars($user['nickname']); ?>
+        </p>
 
-            <p>
-                <?php echo htmlspecialchars($user['email']); ?>
-            </p>
+        <p>
+            <?php echo htmlspecialchars($user['email']); ?>
+        </p>
 
-            <p>
-                <?php echo htmlspecialchars($user['password']); ?>
-            </p>
-
-            <a class="button" href="*">
-                <button>Supprimer l'utilisateur</button>
-            </a>
-        </div>
-    <?php
-    }
-    ?>
+        <a class="button" href="*">
+            <button>Supprimer l'utilisateur</button>
+        </a>
+    </div>
+<?php
+}
+?>
 </body>
 
 </html>
