53416c2557abf411b34b3b60e337c9d433b1a5ef
LeclercqL <104424012+LeclercqL@users.noreply.github.com>
2022-08-03 13:55:42 +0200

    little modif

--- src/view/homepage.php
+++ src/view/homepage.php
@@ -16,103 +16,99 @@ session_start();
 <body>
     <?php require 'include/header.php' ?>
 
-
-
-
-
     <main>
 
         <h2>Bonjour, <?php echo $_SESSION['LOGGED_USER']['firstname']; ?> !</h2>
-        <?php if ($_SESSION['LOGGED_USER']['is_admin'] == "1") { ?>
-            <a class="button" href="admin">
-                <button>Utilisateurs</button>
-            </a>
-        <?php } ?>
-
-        <br>
-        <br>
-
-        <?php
-        foreach ($hikes->getHikes() as $key => $hike) {
-        ?>
-            <div class="hikes">
-
-                <img class="image-hike" src="img/foret.jpeg" alt="A forest" />
-
-                <h3 class=" title">
-                    <?php echo htmlspecialchars($hike['name']); ?>
-                    <em>, le <?php echo date("d-m-Y", strtotime($hike['date'])); ?></em>
-                </h3>
-                <?php if ($hike['date'] != $hike['update_hike']) { ?>
-                    <p class="date">
-                        <em>Modifié le <?php echo date("d-m-Y", strtotime($hike['update_hike'])); ?></em>
-                    </p>
-                <?php } ?>
-
-                <?php
-                foreach ($users->getUser($hike['ID_user']) as $key => $user) {
-                ?>
-
-                    <p class="user"> Par <?php echo htmlspecialchars($user['nickname']); ?>
-                    </p>
-
-                <?php
-                }
-                ?>
-
-                <p class="info">
-                    Distance: <?php echo htmlspecialchars($hike['distance']); ?> km,
-                    dénivelée positif: <?php echo htmlspecialchars($hike['elevation_gain']); ?> m,
-                    durée moyenne: <?php echo htmlspecialchars($hike['duration']); ?>h
-                </p>
-
-                <p class="location">
-                    Départ depuis <?php echo htmlspecialchars($hike['location']); ?>
-                </p>
-
-                <?php
-                foreach ($tags->getTag($hike['ID_tags']) as $key => $tag) {
-                ?>
-
-                    <p class="tags"> Tags: <?php echo htmlspecialchars($tag['name']); ?>
-                    </p>
-
-                    <a class="button" href="hike?id=<?php echo htmlspecialchars($hike['ID']); ?>">
-                        <button>Plus d'info</button>
-                    </a>
-
-                    <?php if ($_SESSION['LOGGED_USER']['is_admin'] == "1") { ?>
-
-                        <a class="button" href="form_update?id=<?php echo htmlspecialchars($hike['ID']); ?>">
-                            <button>Modifier la randonnée</button>
-                        </a>
-                        <a class="button" href="*">
-                            <button>Supprimer la randonnée</button>
-                        </a>
-                    <?php } ?>
-                <?php } ?>
-            </div>
-        <?php
-        }
-        ?>
-
-        </br>
-
-        <?php
-        foreach ($tags->getListTags() as $key => $tag) {
-        ?>
-            <div class="tags">
-
-                <a class="button" href="tag?id=<?php echo htmlspecialchars($tag['ID']); ?>">
-                    <li>
-                        <?php echo htmlspecialchars($tag['name']); ?>
-                    </li>
+        <p> <?php if ($_SESSION['LOGGED_USER']['is_admin'] == "1") { ?>
+                <a class="button" href="admin">
+                    <button class="btn">Utilisateurs</button>
                 </a>
-            </div>
-        <?php
-        }
-        ?>
+        </p>
+    <?php } ?>
 
+    <?php
+    foreach ($tags->getListTags() as $key => $tag) {
+    ?>
+        <div class="tags">
+
+            <a class="button" href="tag?id=<?php echo htmlspecialchars($tag['ID']); ?>">
+                <li>
+                    <?php echo htmlspecialchars($tag['name']); ?>
+                </li>
+            </a>
+        </div>
+    <?php
+    }
+    ?>
+
+    <br>
+    <br>
+
+    <?php
+    foreach ($hikes->getHikes() as $key => $hike) {
+    ?>
+        <div class="hikes">
+
+            <img class="image-hike" src="img/foret.jpeg" alt="A forest" />
+
+            <h3 class="title">
+                <?php echo htmlspecialchars($hike['name']); ?>
+                <em>, le <?php echo date("d-m-Y", strtotime($hike['date'])); ?></em>
+            </h3>
+            <?php if ($hike['date'] != $hike['update_hike']) { ?>
+                <p class="date">
+                    <em>Modifié le <?php echo date("d-m-Y", strtotime($hike['update_hike'])); ?></em>
+                </p>
+            <?php } ?>
+
+            <?php
+            foreach ($users->getUser($hike['ID_user']) as $key => $user) {
+            ?>
+
+                <p class="user"> Par <?php echo htmlspecialchars($user['nickname']); ?>
+                </p>
+
+            <?php
+            }
+            ?>
+
+            <p class="info">
+                Distance: <?php echo htmlspecialchars($hike['distance']); ?> km,
+                dénivelée positif: <?php echo htmlspecialchars($hike['elevation_gain']); ?> m,
+                durée moyenne: <?php echo htmlspecialchars($hike['duration']); ?>h
+            </p>
+
+            <p class="location">
+                Départ depuis <?php echo htmlspecialchars($hike['location']); ?>
+            </p>
+
+            <?php
+            foreach ($tags->getTag($hike['ID_tags']) as $key => $tag) {
+            ?>
+
+                <p class="tags"> Tags: <?php echo htmlspecialchars($tag['name']); ?>
+                </p>
+
+                <a class="button" href="hike?id=<?php echo htmlspecialchars($hike['ID']); ?>">
+                    <button>Plus d'info</button>
+                </a>
+
+                <?php if ($_SESSION['LOGGED_USER']['is_admin'] == "1") { ?>
+
+                    <a class="button" href="form_update?id=<?php echo htmlspecialchars($hike['ID']); ?>">
+                        <button>Modifier la randonnée</button>
+                    </a>
+                    <a class="button" href="*">
+                        <button>Supprimer la randonnée</button>
+                    </a>
+                <?php } ?>
+            <?php } ?>
+        </div>
+    <?php
+    }
+    ?>
+
+    </br>
     </main>
 
     <?php require_once 'include/footer.php'; ?>
